95dd03d1d562a4db3f7bdbfed1faeb8b4bd828e4
Olena Nesteruk <o.nesteruk@ukma.edu.ua>
2025-03-31 16:12:13 +0300

    Added empty functions for input and output

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def input_from_console():
+    pass
+
+def input_from_file(filepath):
+    pass
+
+def input_from_file_with_pandas(filepath):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def output_to_console(text):
+    pass
+
+def output_to_file(filepath, text):
+    pass
+
+def output_to_file_pandas(filepath, data):
+    pass
